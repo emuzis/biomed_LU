664766803bb2257540d0de6a0981acb8f974d4e0
Egils Mūzis <egils.muzis@tieto.com>
2011-04-19 10:47:42 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem "rails", "3.0.5"
+gem "mongrel"
+gem "mongoid", "~> 2.0"
+gem "bson_ext", "~> 1.3"
+gem "SystemTimer"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biomed::Application.config.secret_token = 'e154aa3d1b442a1adb87e83cace1eda74c44fd9450d4214fafe1afd049cf3516ea7da0aee922324187f5e64da867560a08e3104b7a1f9bfbfe58403af08fda59'
